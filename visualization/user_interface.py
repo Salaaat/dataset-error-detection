bbc59b4f7f64e377efae67aaab38fa68e1f89945
Salaaay <camorka.zelena@seznam.cz>
2025-02-22 16:44:51 +0100

    some bug fixes

--- visualization/user_interface.py
+++ visualization/user_interface.py
@@ -21,17 +21,22 @@ def run():
             class_name = class_dictionary.get_class_name(chosen_class)
             print(f"Zvolená třída: {chosen_class},", class_name)
         except Exception as e:
-            print(f"Máme problém: {e}")
+            print(f"Vyskytl se problém: {e}")
 
         #choose probability limit
 
     while not num_of_images:
         try:
-            num_of_images = int(input("Kolik obrázků chcete zobrazit? "))
+            num_of_images = input("Kolik obrázků chcete zobrazit? ")
+            if num_of_images == '':
+                num_of_images = 50  # všechny chybné ve validační sadě
+                print("Zobrazí se tedy všechny chybné obrázky.")
+            else:
+                num_of_images = int(num_of_images)
             if num_of_images <= 0:
                 raise ValueError("Počet obrázků musí být větší než 0.")
         except Exception as e:
-            print(f"Máme problém: {e}")
+            print(f"Vyskytl se problém: {e}")
 
         if not num_of_images:
             num_of_images = 50 #všechny chybné ve validační sadě
@@ -39,17 +44,20 @@ def run():
 
     while not chosen_model:
         try:
-            chosen_model = int(input("Vyberte model jehož predikce využijeme; 0, 1 pro efficientnet (supervised), 2 pro openclip (unsupervised): "))
+            chosen_model = input("Vyberte model jehož predikce využijeme; 0, 1 pro efficientnet (supervised), 2 pro openclip (unsupervised): ")
+            if chosen_model != '':
+                chosen_model = int()
             if not chosen_model in (0, 1, 2):
                 chosen_model = 2
+                print("Použijeme tedy data openclip modelu.")
         except Exception as e:
-            print(f"Máme problém: {e}")
+            print(f"Vyskytl se problém: {e}")
 
         if not chosen_model:
             chosen_model = 2
             print("Použijeme tedy data openclip modelu.")
 
-    label_class_table = pd.read_csv("../loader/imagenet2012_classes_label_match.csv")
+    label_class_table = pd.read_csv("loader/imagenet2012_classes_label_match.csv")
     class_directory = label_class_table.query(f'original_id == {chosen_class}')
     image_displayer.show_images("../imagenet-1k/val/" + class_directory['pt_name'].values[0], num_of_images, chosen_class, class_name, chosen_model)
 
